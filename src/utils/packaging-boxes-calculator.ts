f9e9ee6ab7e824fc60bb9b4b8755150c3e19a9b4
Alisson Luis <alissonluis2009@gmail.com>
2023-08-22 19:56:01 -0300

    check line

--- src/utils/packaging-boxes-calculator.ts
+++ src/utils/packaging-boxes-calculator.ts
@@ -1,7 +1,18 @@
-const totalProductsPerBatch = 2900
 const totalPackagingPerBox = 124
 
-export default function calculatePackagingBoxes(totalBatches: number) {
+export default function calculatePackagingBoxes(totalBatches: number, line: number ) {
+    let totalProductsPerBatch = 0
+
+    if([1, 2].includes(line)) {
+        totalProductsPerBatch = 2900
+    } else if([9, 10].includes(line)) {
+        totalProductsPerBatch = 2000
+    } else if(line == 11) {
+        totalProductsPerBatch = 8000
+    } else {
+        throw new Error("Linha informada é inválida!");
+    }
+    
     const total = Math.round((totalBatches * totalProductsPerBatch) / totalPackagingPerBox)
     return Math.max(total, 1);
 }
